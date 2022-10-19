2ac090263db91fedfdbfdd4989ac1da1936e548c
Nikita Shirobokov <Nik>
2022-10-19 20:11:23 +0400

    Initial Commit

---
+++ FillSliderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FillSliderApp
+//
+//  Created by Nikita Shirobokov on 19.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
